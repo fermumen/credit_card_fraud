54a36a506d6b7528d7cd660bc06a504604a3a621
Fernando Munoz Mendez <fermm.92@gmail.com>
2019-08-25 19:34:44 +0200

    created plumber api file and saved model

---
+++ plumber_api.R
@@ -0,0 +1,6 @@
+#' This scrip loads the model created in the previous file and runs it as a
+#' *Plumber API* so it can predict if a given transaction is fraud or nor.
+#' 
+
+library(mlr)
+library(plumber)
